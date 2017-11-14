b7e27f8d54ce96c50e1759e47463737ed509fe61
johnwmillr <john.w.millr@gmail.com>
2017-11-14 13:27:10 -0600

    Initial commit. Add fraction package

---
+++ fraction/fraction.py
@@ -0,0 +1,41 @@
+def gcd(a, b):
+    while b:
+        a, b = b, a%b
+    return a
+
+class Fraction(object):
+
+    def __init__(self, num, den):
+        self.num = int(num / gcd(abs(num), abs(den)))
+        self.den = int(den / gcd(abs(num), abs(den)))
+
+    def __str__(self):
+        return "{0}/{1}".format(self.num, self.den)
+
+    def __add__(self,f2):
+        return Fraction(self.num*f2.den+f2.num*self.den,self.den*f2.den)
+
+    # TODO: Implement subtraction
+    # def __sub__(self,f2):
+        # return ...
+
+    # TODO: Implement multiplication
+    # def __mul__(self,f2):
+        # return ...
+
+    # TODO: Implement divition
+    # def __div__(self,f2):
+        # return ...
+
+    def __repr__(self):
+        if self.num==self.den:
+            return "1"
+        else:
+            return str(self.num) + "/" + str(self.den)        
+
+    def __float__(self):
+        return float(self.numerator)/float(self.denominator)
+
+    @property
+    def eval(self):
+        return self.__float__()
