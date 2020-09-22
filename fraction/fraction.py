53f626945ea79269fe26122e1dc74ac01449f391
John Miller <john.miller@billups.com>
2020-09-22 13:15:31 -0500

    Apply flake8 formatting

--- fraction/fraction.py
+++ fraction/fraction.py
@@ -1,10 +1,10 @@
 def gcd(a, b):
     while b:
-        a, b = b, a%b
+        a, b = b, a % b
     return a
 
-class Fraction(object):
 
+class Fraction(object):
     def __init__(self, num, den):
         self.num = int(num / gcd(abs(num), abs(den)))
         self.den = int(den / gcd(abs(num), abs(den)))
@@ -12,29 +12,30 @@ class Fraction(object):
     def __str__(self):
         return "{0}/{1}".format(self.num, self.den)
 
-    def __add__(self,f2):
-        return Fraction(self.num*f2.den+f2.num*self.den,self.den*f2.den)
+    def __add__(self, f2):
+        return Fraction(self.num * f2.den + f2.num * self.den,
+                        self.den * f2.den)
 
     # TODO: Implement subtraction
     # def __sub__(self,f2):
-        # return ...
+    # return ...
 
     # TODO: Implement multiplication
     # def __mul__(self,f2):
-        # return ...
+    # return ...
 
     # TODO: Implement divition
     # def __div__(self,f2):
-        # return ...
+    # return ...
 
     def __repr__(self):
-        if self.num==self.den:
+        if self.num == self.den:
             return "1"
         else:
-            return str(self.num) + "/" + str(self.den)        
+            return str(self.num) + "/" + str(self.den)
 
     def __float__(self):
-        return float(self.num)/float(self.den)
+        return float(self.num) / float(self.den)
 
     @property
     def eval(self):
